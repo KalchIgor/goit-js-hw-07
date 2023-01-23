d373dbb3b2810613ae46a9f5bcead4d86e7b1bdc
Игорь Кальчин <108684879+KalchIgor@users.noreply.github.com>
2023-01-23 14:24:54 +0200

    Update 01-gallery.js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -8,8 +8,10 @@ const items = [];
 
  modal.show();
 
-    function onEscKeyPress(evt) {   
-        if (evt.code === "Escape") {
+function onEscKeyPress(evt) {   
+    const ESC_KEY_CODE = "Escape";
+    const evt = evt.code === ESC_KEY_CODE;
+        if (evt) {
             modal.close();
         }
     }

bdf9fb21cd36c9d5b1161faff2341b37adcb5f0e
Игорь Кальчин <108684879+KalchIgor@users.noreply.github.com>
2023-01-23 13:51:24 +0200

    Update 01-gallery.js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,14 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const gallery = document.querySelector('.gallery');
+const items = [];
+
+ modal.show();
+
+    function onEscKey(evt) {   
+        if (evt.code === "Escape") {
+            modal.close();
+        }
+    }

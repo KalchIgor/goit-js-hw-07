68a8f12bf471dd06bf994fb43914be2739af9319
Игорь Кальчин <108684879+KalchIgor@users.noreply.github.com>
2023-01-24 15:53:02 +0200

    Update 01-gallery.js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,47 +1,39 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-// console.log(galleryItems);
+//console.log(galleryItems);
 
-const gallery = document.querySelector('.gallery');
+const gallery = document.querySelector(".gallery");
 
+const galaryElem = galleryItems
+    .map(
+        (item) =>
+            `<div class="gallery__item"><a class="gallery__link" href=${item.original}><img class="gallery__image" src=${item.preview} data-source=${item.original} alt=${item.description}/></a></div>`
+    )
+    .join("");
+gallery.innerHTML = galaryElem;
 
-const galaryElem = galleryItems.map(({ preview, original,
-    description }) => {`
-    <div class="gallery__item">
-     <a class="gallery__link"  href="${original}>
-    <img
-      class="gallery__image" 
-      src="${ preview}"
-      data-source="${original}"
-      alt="${description}"
-    />
-  </a>
-    </div>`
-    }).join('');
+let modalCreate;
 
-gallery.insertAdjacentHTML('beforeend', galleryElem);
-
-function onClicImg(evn) {
-    evt.preventDefault();
-     if (evt.target.nodeName !== 'IMG') {
+gallery.addEventListener("click", (e) => {
+    e.preventDefault();
+    if (e.target.nodeName !== "IMG") {
         return;
-     }
-        const modal = basicLightbox.create(
-            `<img src="${evt.target.dataset.source}" width="800" height="600">`,
-                    {   onShow: () => window.addEventListener('keydown', onEscKeyPress),
-            onClose: () => window.removeEventListener('keydown', onEscKeyPress),
-        }
-    );
-}
-
-
- modal.show();
-
-function onEscKeyPress(evt) {   
-    const ESC_KEY_CODE = "Escape";
-    const evt = evt.code === ESC_KEY_CODE;
-        if (evt) {
-            modal.close();
-        }
     }
+    const modal = document.createElement("div");
+    const newPic = document.createElement("img");
+    newPic.src = e.target.getAttribute("data-source");
+    modal.append(newPic);
+
+    modalCreate = basicLightbox.create(modal);
+    
+    modalCreate.show();
+});
+
+document.addEventListener("keydown", (event) => {
+    if (event.key === "Escape") {
+        modalCreate.close();
+    }
+});
+
+

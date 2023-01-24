a706170046eaeb516e118be7d9484ca968e962e2
Игорь Кальчин <108684879+KalchIgor@users.noreply.github.com>
2023-01-24 16:55:53 +0200

    Update 02-lightbox.js

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,7 +1,22 @@
 import { galleryItems } from './gallery-items.js';
+
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
 
-const galery = document.querySelector(".gallery");
 
+const gallery = document.querySelector(".gallery");
+
+const galleryImg = galleryItems
+    .map(
+        (item) =>
+            `<a class="gallery__item" href=${item.original}><img class="gallery__image" src=${item.preview} alt=${item.description}/></a>`
+    )
+    .join("");
+    
+gallery.innerHTML = galleryImg;
+
+let lightbox = new SimpleLightbox(".gallery a", {
+    captionsData: "alt",
+    captionDelay: 250,
+});

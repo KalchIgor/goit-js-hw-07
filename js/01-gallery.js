424dd7707f56d4efce7efa711fda760644f5cec7
Игорь Кальчин <108684879+KalchIgor@users.noreply.github.com>
2023-01-24 14:25:33 +0200

    Update 01-gallery.js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,10 +1,26 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
 
 const gallery = document.querySelector('.gallery');
-const items = [];
+
+
+const galaryElem = galleryItems.map(({ preview, original,
+    description }) => {`
+    <div class="gallery__item">
+     <a class="gallery__link"  href="${original}>
+    <img
+      class="gallery__image" 
+      src="${ preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+    </div>`
+    }).join('');
+
+
 
  modal.show();
 
